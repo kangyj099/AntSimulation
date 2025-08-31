14e123ff2eec22d24ff08785c4000104950e11d7
kangyj099 <kang.yj099@gmail.com>
2025-08-31 21:07:28 +0900

    feat: 개미 Ant 클래스 추가

---
+++ AntSimulation/Ant.cpp
@@ -0,0 +1,44 @@
+﻿module ant;
+
+import common;
+import utils;
+
+import movement;
+
+Ant::Ant(Field& _field) : GameObject(_field)
+{
+}
+
+Ant::~Ant()
+{
+}
+
+void Ant::OnUpdate()
+{
+}
+
+void Ant::OnDraw()
+{
+	ConsolePos consolePos = Utils::FieldPositionToConsolePos(pos);
+	Utils::FillTile(consolePos, Constants::Color::Magenta);
+}
+
+void Ant::Reset()
+{
+}
+
+void Ant::Setting(FieldPos _pos, std::string _name, float _weight)
+{
+	pos = _pos;
+	name = _name;
+	weightMG = _weight;
+}
+
+void Ant::Init()
+{
+	Movement* movement = AddComponent<Movement>(pos);
+	if (nullptr == movement)
+	{
+		__debugbreak();
+	}
+}
---
+++ AntSimulation/Ant.ixx
@@ -0,0 +1,26 @@
+﻿export module ant;
+
+import gameObject;
+import movement;
+
+export class Ant : public GameObject
+{
+private:
+
+public:
+	Ant(Field& _field);
+	~Ant() override;
+
+	virtual void OnUpdate() override;
+	virtual void OnDraw() override;
+
+	virtual void Reset() override;
+	void Setting(FieldPos _pos, std::string _name, float _weight);
+
+	void Init();
+
+	void SetPos(short _x, short _y)
+	{
+		pos.Set(_x, _y);
+	}
+};
